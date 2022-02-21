8b53ebb78e8791d0fa68fbc00370a5a43102142c
Andreas <andrerim@stud.ntnu.no>
2022-02-21 22:30:49 +0100

    Scrape data

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import * as puppeteer from "puppeteer";
 
-console.log(puppeteer);
-
-(async () => {
+async function scrapeData() {
+    const kantineUrl = "https://play.loopsign.eu/app/218/498351";
     const browser = await puppeteer.launch({});
     const page = await browser.newPage();
-    await page.goto("https://example.com");
-    await page.screenshot({ path: "example.png" });
-
+    await page.goto(kantineUrl);
+    await page.waitForSelector(`div[data-contents="true"]`);
+    //await page.screenshot({ path: "menu.png" });
+    const r = await page.$$(`span[data-text="true"]`);
+    for (const el of r) {
+        console.log((await el.getProperty("innerHTML")).toString());
+    }
     await browser.close();
-})();
+    console.log("finished");
+}
+
+scrapeData();
